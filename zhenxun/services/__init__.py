d168eeff3c2de7cdfe2d73180b06d5915bd62a8c
harry <hustharry@foxmail.com>
2025-02-21 10:09:23 +0800

    change ai config

--- zhenxun/services/__init__.py
+++ zhenxun/services/__init__.py
@@ -1,7 +1,9 @@
 from nonebot import require
+from zhenxun.services.log import logger
 
 require("nonebot_plugin_apscheduler")
 require("nonebot_plugin_alconna")
 require("nonebot_plugin_session")
 require("nonebot_plugin_htmlrender")
 require("nonebot_plugin_uninfo")
+
